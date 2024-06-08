e85133f36c0ea6f189de7a80e2777265d01c15f2
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-06-08 11:41:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const textareaEl = document.getElementById("textarea");
-const totalCounterEl = document.getElementById("total-counter")
+const totalCounterEl = document.getElementById("total-counter");
+const remainingCounterEl = document.getElementById("Remaining");
 
-textareaEl.addEventListener("keyup" , ()=>{
-    updateCounter()
-})
+textareaEl.addEventListener("keyup", () => {
+  updateCounter();
+});
 
-function updateCounter(){
-  totalCounterEl.innerText =  textareaEl.value.length
+function updateCounter() {
+  totalCounterEl.innerText = textareaEl.value.length;
+  remainingCounterEl.innerText = textareaEl.getAttribute("maxlength") - textareaEl.value.length
 }
